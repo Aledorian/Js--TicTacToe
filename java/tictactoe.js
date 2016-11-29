51d2e114358c406878438a2d3055b39cb4ab37b0
simplon <aledorianlegrand@gmail.com>
2016-11-29 12:01:27 +0100

    fin

--- java/tictactoe.js
+++ java/tictactoe.js
@@ -4,21 +4,80 @@ for(var i=0; i<9; i++){
   cells[i].onclick = tour;
 }
 var t = 0;
+var p = 1
+var win = document.getElementById('winner')
 
 function tour(e){
-  if (t === 0 || t === 2 || t === 4 || t === 6 || t === 8) {
+
+  if (e.target.className == "player1" || e.target.className == "player2") {
+    return;
+  }
+
+  if (p === 1) {
     document.getElementById('joueur').innerHTML = "P1";
     e.target.className = 'player1';
+    p++;
   }
-  else if (t === 1 || t === 3 || t === 5 || t === 7){
+  else if (p === 2){
     document.getElementById('joueur').innerHTML = "P2";
     e.target.className = 'player2';
-  }
-  else{
-    document.getElementById('joueur').innerHTML = "Fin";
+    p--;
   }
   t++;
   console.log(t);
+
+//____________________________________________________Victory ?______________________________________//
+
+  if (cells[0].className == "player1" && cells[1].className == "player1" && cells[2].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[3].className == "player1" && cells[4].className == "player1" && cells[5].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[6].className == "player1" && cells[7].className == "player1" && cells[8].className == "player1") {
+    win.className = "winnerp1"
+    }
+  else if (cells[0].className == "player1" && cells[3].className == "player1" && cells[6].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[1].className == "player1" && cells[4].className == "player1" && cells[7].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[2].className == "player1" && cells[5].className == "player1" && cells[8].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[0].className == "player1" && cells[4].className == "player1" && cells[8].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[2].className == "player1" && cells[4].className == "player1" && cells[6].className == "player1") {
+    win.className = "winnerp1"
+  }
+  else if (cells[0].className == "player2" && cells[1].className == "player2" && cells[2].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[3].className == "player2" && cells[4].className == "player2" && cells[5].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[6].className == "player2" && cells[7].className == "player2" && cells[8].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[0].className == "player2" && cells[3].className == "player2" && cells[6].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[1].className == "player2" && cells[4].className == "player2" && cells[7].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[2].className == "player2" && cells[5].className == "player2" && cells[8].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[0].className == "player2" && cells[4].className == "player2" && cells[8].className == "player2") {
+    win.className = "winnerp2"
+  }
+  else if (cells[2].className == "player2" && cells[4].className == "player2" && cells[6].className == "player2") {
+    win.className = "winnerp2"
+  }
 }
 
-if
+function oust(){
+  win.className = "1"
+}
